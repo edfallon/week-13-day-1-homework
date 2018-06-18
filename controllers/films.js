436f9f2c9c46c0db0009498660a6962306f2ab97
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-18 17:16:10 +0100

    completed homework

---
+++ controllers/films.js
@@ -0,0 +1,36 @@
+const films = require('../models/films')();
+const Film = require('../models/film');
+const Review = require('../models/review');
+const express = require('express');
+const filmRouter = new express.Router();
+
+filmRouter.get('/:id', function (req, res){
+  const index = req.params.id;
+  res.json({data: films[index]})
+});
+
+filmRouter.delete('/:id', function(req, res){
+  const index = req.params.id;
+  films.splice(index, 1);
+  res.json({data: films});
+});
+
+filmRouter.get('/', function(req, res){
+  res.json(films);
+});
+
+filmRouter.put('/:id', function(req, res){ //update
+  const index = req.params.id;
+  films[index] = new Film(req.body);
+  res.json({data:films});
+});
+
+filmRouter.post('/', function(req, res){
+  const newFilm = new Film(
+    req.body
+  );
+  films.push(newFilm);
+  res.json({data: films});
+});
+
+module.exports = filmRouter;
